af3a9e064469ded91b6ec2736a715b0c55c625ab
Joaquin <55727778+bl4ck-cl@users.noreply.github.com>
2020-08-30 06:06:12 -0400

    Sidebar añadido

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
-## TESIS PULENTA
+## TESIS PULENTA:^)
 
 In the project directory, you can run:
 
--- src/App.js
+++ src/App.js
@@ -1,38 +1,26 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-
-
-import {
-  withScriptjs,
-  withGoogleMap,
-  GoogleMap,
-  Marker,
-} from "react-google-maps";
+import { withScriptjs, withGoogleMap, GoogleMap, Marker, } from "react-google-maps";
 
 const MyMapComponent = withScriptjs(
   withGoogleMap((props) => (
     <GoogleMap
       defaultZoom={8}
-      defaultCenter={{ lat: -33.566593, lng: -70.548037 }}>
+      defaultCenter={{ lat: -33.566593, lng: -70.548037 }}
+      defaultOptions={{
+        streetViewControl: false,
+        scaleControl: false,
+        mapTypeControl: false,
+        panControl: false,
+        zoomControl: true,
+        rotateControl: true,
+        fullscreenControl: false,
+      }}
+      disableDefaultUI
+    >
       <Marker position={{ lat: -33.566593, lng: -70.548037 }} />
     </GoogleMap>
   ))
 );
 
-
-function App() {
-  return (
-    <div className="App">
-      <MyMapComponent
-        isMarkerShown
-        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCVACQHjPUjNm2kQ9ova72zU8iLxGBAoa4&v=3.exp&libraries=geometry,drawing,places"
-        loadingElement={<div style={{ height: `100%` }} />}
-        containerElement={<div style={{ height: `400px` }} />}
-        mapElement={<div style={{ height: `100%` }} />}
-      />
-    </div>
-  );
-}
-
-export default App;
+export default MyMapComponent;
